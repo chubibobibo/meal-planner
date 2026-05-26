fd2d4b1f5f693a72821727ab473056e836751bf2
Alistair Abao <abaolester@gmail.com>
2026-05-26 23:02:58 +0200

    fixed adding meals and disabling button after sunday

--- main.py
+++ main.py
@@ -14,15 +14,22 @@ def main(page: Page):
 
 
     # @add_meal = adds a Text element with value from texInput
-    counter = 1
+    counter = 0
+    days = {0: 'Monday', 1: 'Tuesday', 2: 'Wednesday', 3: 'Thursday', 4: 'Friday', 5: 'Saturday', 6: 'Sunday', 7: 'Press reset to start again'}
     
     def add_meal(e):
         nonlocal counter
-        days = {1: 'Tuesday', 2: 'Wednesday', 3: 'Thursday', 4: 'Friday', 5: 'Saturday', 6: 'Sunday'}
         page.add(ft.Text(meal_input.value))
         meal_input.value=''
-        meal_input.hint_text = f'Enter meal for {days[counter]}'
         counter = counter + 1
+        meal_input.hint_text = f'Enter meal for {days[counter]}'
+        if counter > 6:
+            meal_input.hint_text = 'Press reset to enter a new meal for the week'
+            meal_input.disabled=True 
+            add_meal_btn.disabled=True
+            add_meal_btn.bgcolor='grey200'
+            page.add(ft.FloatingActionButton(icon=Icons.RESET_TV))
+            counter = None   
         print(counter)
         page.update()
         
@@ -36,8 +43,11 @@ def main(page: Page):
 
     
     # define input elements
-    add_meal_btn = ft.FloatingActionButton(icon=Icons.ADD, on_click=add_meal)
-    meal_input = ft.TextField(hint_text=f'Enter the meal for Monday')
+    try:
+        add_meal_btn = ft.FloatingActionButton(icon=Icons.ADD, on_click=add_meal)
+        meal_input = ft.TextField(hint_text=f'Enter the meal for {days[counter]}')
+    except:
+        print('Something went wrong')
 
 
   
